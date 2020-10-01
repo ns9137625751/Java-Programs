0b28a6ff139d51f2b18a338e3e7e2d3321428f07
ankitbholu <72176847+ankitbholu@users.noreply.github.com>
2020-10-01 08:15:42 +0530

    Num.java

---
+++ num.java
@@ -0,0 +1,14 @@
+# https://www.facebook.com/permalink.php?story_fbid=1284733925212917&id=100010289646874
+# Subscribed by ARS Army
+
+package com.company;
+
+public class add_03_num {
+    public static void main(String[] args){
+        float subject1=15;
+        float subject2=35;
+        float subject3=50;
+        float cgpa = (subject1 + subject2 + subject3)/30;
+        System.out.println(cgpa);
+    }
+}
