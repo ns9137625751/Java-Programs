b4a154f69c36597de39f49736dafa5caeb892ff0
Deepak Singh Bhainsora <32154563+deepaksbmj@users.noreply.github.com>
2020-10-02 19:32:33 +0530

    Update ReadJSONExample.java

--- Json Read & Write/src/main/java/com/howtodoinjava/demo/jsonsimple/ReadJSONExample.java
+++ Json Read & Write/src/main/java/com/howtodoinjava/demo/jsonsimple/ReadJSONExample.java
@@ -1,5 +1,3 @@
-package com.howtodoinjava.demo.jsonsimple;
-
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -14,18 +12,15 @@ public class ReadJSONExample
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) 
 	{
-		//JSON parser object to parse read file
 		JSONParser jsonParser = new JSONParser();
 		
 		try (FileReader reader = new FileReader("employees.json"))
 		{
-			//Read JSON file
             Object obj = jsonParser.parse(reader);
 
             JSONArray employeeList = (JSONArray) obj;
             System.out.println(employeeList);
             
-            //Iterate over employee array
             employeeList.forEach( emp -> parseEmployeeObject( (JSONObject) emp ) );
 
         } catch (FileNotFoundException e) {
@@ -39,18 +34,11 @@ public class ReadJSONExample
 
 	private static void parseEmployeeObject(JSONObject employee) 
 	{
-		//Get employee object within list
 		JSONObject employeeObject = (JSONObject) employee.get("employee");
-		
-		//Get employee first name
 		String firstName = (String) employeeObject.get("firstName");	
 		System.out.println(firstName);
-		
-		//Get employee last name
 		String lastName = (String) employeeObject.get("lastName");	
 		System.out.println(lastName);
-		
-		//Get employee website name
 		String website = (String) employeeObject.get("website");	
 		System.out.println(website);
 	}
