65aa0dc6fc3c5fe123031c9ac6feae3655000571
Deepak Singh Bhainsora <32154563+deepaksbmj@users.noreply.github.com>
2020-10-02 19:33:49 +0530

    Update WriteJSONExample.java

--- Json Read & Write/src/main/java/com/howtodoinjava/demo/jsonsimple/WriteJSONExample.java
+++ Json Read & Write/src/main/java/com/howtodoinjava/demo/jsonsimple/WriteJSONExample.java
@@ -1,5 +1,3 @@
-package com.howtodoinjava.demo.jsonsimple;
-
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -11,30 +9,25 @@ public class WriteJSONExample
     @SuppressWarnings("unchecked")
 	public static void main( String[] args )
     {
-    	//First Employee
     	JSONObject employeeDetails = new JSONObject();
-    	employeeDetails.put("firstName", "Lokesh");
-    	employeeDetails.put("lastName", "Gupta");
-    	employeeDetails.put("website", "howtodoinjava.com");
+    	employeeDetails.put("firstName", "Deepak");
+    	employeeDetails.put("lastName", "Singh");
+    	employeeDetails.put("website", "google.com");
     	
     	JSONObject employeeObject = new JSONObject(); 
     	employeeObject.put("employee", employeeDetails);
     	
     	//Second Employee
     	JSONObject employeeDetails2 = new JSONObject();
-    	employeeDetails2.put("firstName", "Brian");
-    	employeeDetails2.put("lastName", "Schultz");
+    	employeeDetails2.put("firstName", "Dexter");
+    	employeeDetails2.put("lastName", "Jackson");
     	employeeDetails2.put("website", "example.com");
     	
     	JSONObject employeeObject2 = new JSONObject(); 
     	employeeObject2.put("employee", employeeDetails2);
-    	
-    	//Add employees to list
     	JSONArray employeeList = new JSONArray();
     	employeeList.add(employeeObject);
     	employeeList.add(employeeObject2);
-    	
-    	//Write JSON file
     	try (FileWriter file = new FileWriter("employees.json")) {
 
             file.write(employeeList.toJSONString());
